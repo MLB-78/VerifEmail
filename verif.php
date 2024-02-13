99d40ebf95ca7ddd208bd42261480b4c6b5abd08
Maxime <100378645+MLB-78@users.noreply.github.com>
2024-02-13 16:53:13 +0100

    Update verif.php

--- verif.php
+++ verif.php
@@ -1,40 +1,39 @@
 <?php
-// Fonction pour vérifier la sécurité d'une adresse e-mail
+// Fonction pour vérifier la sécurité d'une adresse e-mail avec hunter.io
 function verifierSecuriteEmail($email) {
     $api_key = "3f58cbf5e2a0294fbdbcdf934e59f322c3bd0bb0";
     $url = "https://api.hunter.io/v2/email-verifier?email=" . urlencode($email) . "&api_key=" . $api_key;
 
-    // Initialiser cURL
+    // Initialisation
     $ch = curl_init($url);
 
-    // Configuration des options de cURL
+    // Configuration des options cURL
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    // Exécution de la requête
+    // Exécution de la req
     $response = curl_exec($ch);
 
-    // Gestion des erreurs
+    // return si il y a une erreur
     if (curl_errno($ch)) {
         echo 'Erreur cURL : ' . curl_error($ch);
     }
 
-    // Fermeture de la session cURL
+    // Fermeture de la session 
     curl_close($ch);
 
-    // Décodage de la réponse JSON
+    
     $result = json_decode($response, true);
 
     // Vérifier si la requête a réussi
     if ($result && isset($result['data']['result']) && $result['data']['result'] == 'valid') {
-        return true; // L'e-mail est considéré comme sécurisé
+        return true; 
     } else {
-        return false; // L'e-mail n'est pas considéré comme sécurisé
+        return false; 
     }
 }
 
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer la valeur de l'e-mail depuis le formulaire
     $email_a_verifier = isset($_POST["email"]) ? $_POST["email"] : '';
 
     // Vérifier si l'e-mail est présent avant d'appeler la fonction
